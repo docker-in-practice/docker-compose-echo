e6d37747a111a1f3434121cbed3446dd58071b0a
ianmiell <ian.miell@gmail.com>
2015-03-03 14:10:01 +0000

    More reliable and better naming

--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@ import socket, time, sys
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('talkto',2000))
-    s.send('Hello,world\n')
+    s.send('Hello, world\n')
     data = s.recv(1024)
     print 'Received:', data 
     sys.stdout.flush()
